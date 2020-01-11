c9104bd258acdb51f7085fc490f6ff1df11cbc59
Rajat <rajats1.kv2srg@gmail.com>
2020-01-12 01:10:31 +0530

    static version

---
+++ RecipeApp/src/Recipe.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './Recipe.css';
+
+class RecipeApp extends Component {
+	render(){
+		const {title, img, instructions} = this.props;
+		const ingredients = this.props.ingredients.map((ing, index)=> (<li key={index}>{ing}</li>));
+		return (
+			<div className="recipe-card">
+				<div className="recipe-card-image">
+					<img src={img} alt={title}/>
+				</div>
+
+				<div className="recipe-card-content">
+					<h3 class="recipe-title">{title}</h3>
+					<h4> Ingredients </h4>
+					<ul>
+						{ingredients}
+					</ul>
+					<h4>instructions</h4>
+					<p>{instructions}</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default RecipeApp;
