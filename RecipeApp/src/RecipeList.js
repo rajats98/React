9cf8233775c04498d864db0ac1a0839e61383e53
Rajat <rajats1.kv2srg@gmail.com>
2020-01-12 18:41:16 +0530

    PropTypes

--- RecipeApp/src/RecipeList.js
+++ RecipeApp/src/RecipeList.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './RecipeList.css';
 import Recipe from './Recipe';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class RecipeList extends Component {
-	
+	static propTypes = {
+		recipes: PropTypes.arrayOf(PropTypes.object).isRequired,
+		onDelete: PropTypes.func.isRequired
+	}
+		
 	render(){
 		const recipes = this.props.recipes.map((r,i)=> (<Recipe key={r.id} {...r} onDelete={this.props.onDelete} />));
 		return (
