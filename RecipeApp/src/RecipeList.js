cf89e99a87ec36c0ee0b78a027494919d3bbd36a
Rajat <rajats1.kv2srg@gmail.com>
2020-01-12 15:28:39 +0530

    correction

--- RecipeApp/src/RecipeList.js
+++ RecipeApp/src/RecipeList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './RecipeList.css';
 import Recipe from './Recipe';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class RecipeList extends Component {
 	
