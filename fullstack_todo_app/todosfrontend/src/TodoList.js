f16c2e162125521325d4eb86862bd8f55b497d4d
Rajat <rajats1.kv2srg@gmail.com>
2020-01-16 01:33:34 +0530

    Add toggle-todo

--- fullstack_todo_app/todosfrontend/src/TodoList.js
+++ fullstack_todo_app/todosfrontend/src/TodoList.js
@@ -88,13 +88,46 @@ class TodoList extends Component{
 	 		this.setState({todos});
 	 	})
 	}
-
+	toggleTodo(todo){	
+		const updateURL = APIURL+todo._id;
+		fetch(updateURL,{
+			method:'put',
+			headers: new Headers({
+				'Content-Type':'application/json' 
+			}),
+			body: JSON.stringify({completed: !todo.completed})
+		})
+		.then(res =>{
+	 		if(!res.ok){
+	 			if(res.status>=400 && res.status<500){
+	 				return res.json().then(data => {
+	 					let err = {errorMessage: data.message};
+	 					throw err;
+	 				})
+	 			}
+	 			else {
+	 				let err = {errorMessage:'Please try again later, Serve is not responding'};
+	 				throw err; 
+	 			}
+	 		}
+	 		return res.json();
+	 	})
+	 	.then(updatedTodo=>{
+	 		const todos = this.state.todos.map(t=>
+	 			(t._id===updatedTodo._id)
+	 			? {...t,completed:!t.completed}
+	 			:t
+	 		)
+	 		this.setState({todos});
+	 	})
+	}
 	render() {
 		const todos = this.state.todos.map(t => (
 			<TodoItem
 				key={t._id}
 				{...t}
 				onDelete = {this.deleteTodo.bind(this,t._id)}
+				onToggle = {this.toggleTodo.bind(this,t)}
 			/>
 		))
 		return (
