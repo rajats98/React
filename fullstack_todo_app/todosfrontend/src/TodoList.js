ca1f9919caa91b31a4e9c62b41424d365ff9cc1c
Rajat <rajats1.kv2srg@gmail.com>
2020-01-16 01:13:11 +0530

    Add delete-todo option

--- fullstack_todo_app/todosfrontend/src/TodoList.js
+++ fullstack_todo_app/todosfrontend/src/TodoList.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm'
 
-const APIURL ='/api/todos';
+const APIURL ='/api/todos/';
 class TodoList extends Component{
 	constructor(props){
 		super(props);
@@ -62,11 +62,39 @@ class TodoList extends Component{
 		});
 
 	}
+
+	deleteTodo(id){
+		const deleteURl = APIURL+id;
+		fetch(deleteURl,{
+			method:'delete'
+		})
+		.then(res =>{
+	 		if(!res.ok){
+	 			if(res.status>=400 && res.status<500){
+	 				return res.json().then(data => {
+	 					let err = {errorMessage: data.message};
+	 					throw err;
+	 				})
+	 			}
+	 			else {
+	 				let err = {errorMessage:'Please try again later, Serve is not responding'};
+	 				throw err; 
+	 			}
+	 		}
+	 		return res.json();
+	 	})
+	 	.then(()=>{
+	 		const todos = this.state.todos.filter((todo)=>todo._id!==id)
+	 		this.setState({todos});
+	 	})
+	}
+
 	render() {
 		const todos = this.state.todos.map(t => (
 			<TodoItem
 				key={t._id}
 				{...t}
+				onDelete = {this.deleteTodo.bind(this,t._id)}
 			/>
 		))
 		return (
