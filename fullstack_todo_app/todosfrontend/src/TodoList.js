8ae14d509802725afc8756986bc993d63360f156
Rajat <rajats1.kv2srg@gmail.com>
2020-01-15 23:42:14 +0530

    add TodoItem Component

--- fullstack_todo_app/todosfrontend/src/TodoList.js
+++ fullstack_todo_app/todosfrontend/src/TodoList.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import TodoItem from './TodoItem';
 const APIURL ='/api/todos';
 class TodoList extends Component{
 	constructor(props){
@@ -31,10 +32,18 @@ class TodoList extends Component{
 		 .then(todos => this.setState({todos}));
 	}
 	render() {
-
+		const todos = this.state.todos.map(t => (
+			<TodoItem 
+				key={t._id}
+				{...t}
+			/>
+		))
 		return (
 			<div>
 				<h1>TodoList</h1>
+				<ul>
+					{todos}
+				</ul>
 			</div>
 		);
 	}
