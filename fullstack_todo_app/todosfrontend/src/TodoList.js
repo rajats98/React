586bba46fda11fe55fc3333791395f2aac866e40
Rajat <rajats1.kv2srg@gmail.com>
2020-01-15 23:26:35 +0530

    load initial data

--- fullstack_todo_app/todosfrontend/src/TodoList.js
+++ fullstack_todo_app/todosfrontend/src/TodoList.js
@@ -1,12 +1,42 @@
 import React, {Component} from 'react';
-
+const APIURL ='/api/todos';
 class TodoList extends Component{
-	
+	constructor(props){
+		super(props);
+		this.state = {
+			todos:[]
+		}
+	}
+	componentDidMount(){
+		this.loadTodos();
+	}
+
+	loadTodos(){
+		fetch(APIURL)
+		 .then(res =>{ 
+		 		if(!res.ok){
+		 			if(res.status>=400 && res.status<500){
+		 				return res.json().then(data => {
+		 					let err = {errorMessage: data.message};
+		 					throw err;
+		 				})
+		 			}
+		 			else {
+		 				let err = {errorMessage:'Please try again later, Serve is not responding'};
+		 				throw err; 
+		 			}
+		 		}
+		 		return res.json();
+		 	})
+		 .then(todos => this.setState({todos}));
+	}
 	render() {
+
 		return (
 			<div>
-				<h1>TodoList<h1>
+				<h1>TodoList</h1>
 			</div>
 		);
 	}
 }
+export default TodoList;
