0af3435b523fc02910a5dfc2c072446cd041fd73
Rajat <rajats1.kv2srg@gmail.com>
2020-01-21 01:46:17 +0530

    counter created

---
+++ Redux/redux-counter/script.js
@@ -0,0 +1,39 @@
+// reducer (rootReducer)
+// redux store and initialState
+// we need some way of changing the state
+
+const initialState = {
+	count:0
+}
+function rootReducer(state=initialState,action) {
+	switch(action.type){
+		case "INCREMENT":
+			var newState = {...state};
+			newState.count++;
+			return newState;
+		case "DECREMENT":
+			var newState = {...state};
+			newState.count--;
+			return newState;
+		default:
+			return state; 
+	}
+}
+
+const store = Redux.createStore(rootReducer);
+
+$(document).ready(function(){
+	let currentState = store.getState();
+	$('#counter').text(currentState.count);	
+	
+	$("#increment").on("click",function(){
+		store.dispatch({type:'INCREMENT'});
+		let currentState = store.getState();
+		$('#counter').text(currentState.count);
+	})
+	$("#decrement").on("click",function(){
+		store.dispatch({type:'DECREMENT'});
+		let currentState = store.getState();
+		$('#counter').text(currentState.count);
+	})
+})
